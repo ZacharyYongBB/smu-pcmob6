8ce0bd4e9286036d77d186f1384e2ec2fb1b7f74
ZacharyYongBB <zacharyyong_1997@outlook.com>
2022-07-05 09:42:24 +0800

    added edit post function

--- screens/DetailsScreen.js
+++ screens/DetailsScreen.js
@@ -49,7 +49,7 @@ export default function ShowScreen({ navigation, route }) {
 
 
   function editPost() {
-    navigation.navigate("Edit");
+    navigation.navigate("Edit", { post: post })
   }
   
   return (
